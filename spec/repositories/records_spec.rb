58ec911164837e084d338dffee9ec6853cf41a93
Alexandre Santos <alexandre@bankfacil.com.br>
2016-07-12 16:18:26 -0300

    Add repository to interface ad

---
+++ spec/repositories/records_spec.rb
@@ -0,0 +1,155 @@
+require 'spec_helper'
+
+describe ZohoCrm::Repositories::Records do
+  it 'defines model' do
+    expect(repository_class.model_class).to eq(model_class)
+  end
+
+  it 'defines record adapter by default' do
+    expect(repository_class.adapter).to be_kind_of(ZohoCrm::Adapters::Record)
+  end
+
+  it 'sets adapter' do
+    adapter = double
+    repository_class.adapter = adapter
+
+    expect(repository_class.adapter).to eq(adapter)
+  end
+
+  describe 'finding a record' do
+    it 'finds a record' do
+      adapter = double
+      repository_class.adapter = adapter
+      allow(adapter).to receive(:get_record) { { id: 1 } }
+
+      result = repository_class.new.find(1)
+      expect(result.id).to eq(1)
+    end
+
+    it 'returns nil for inexistent record' do
+      adapter = double
+      repository_class.adapter = adapter
+      allow(adapter).to receive(:get_record) { nil }
+
+      result = repository_class.new.find(1)
+      expect(result).to be_nil
+    end
+  end
+
+  it 'creates a record' do
+    adapter = double
+    repository_class.adapter = adapter
+    allow(adapter).to receive(:insert_record) { { id: 1 } }
+
+    result = repository_class.new.create(model_class.new)
+    expect(result.id).to eq(1)
+  end
+
+  it 'updates a record' do
+    adapter = double
+    repository_class.adapter = adapter
+    allow(adapter).to receive(:update_record) { { id: 2 } }
+
+    result = repository_class.new.update(model_class.new)
+    expect(result.id).to eq(2)
+  end
+
+  describe 'saving' do
+    context 'new record' do
+      it 'creates record' do
+        repo = repository_class.new
+        expect(repo).to receive(:create)
+
+        repo.save(model_class.new(id: nil))
+      end
+    end
+
+    context 'existing record' do
+      it 'updates record' do
+        repo = repository_class.new
+        expect(repo).to receive(:update)
+
+        repo.save(model_class.new(id: 1))
+      end
+    end
+  end
+
+  describe 'searching' do
+    context 'with conditions' do
+      it 'fetches data by criteria' do
+        adapter = double
+        repository_class.adapter = adapter
+
+        repo = repository_class.new
+        repo.select(:id).from(0).to(10).order(:id, :asc).where(id: 1)
+
+        expect(adapter).to receive(:search_records).with({
+          "selectColumns" => "MyModule(ID)",
+          "fromIndex" => 0,
+          "toIndex" => 10,
+          "sortColumnString" => "ID",
+          "sortOrderString" => "asc",
+          "criteria"=>"((ID:1))"
+        }) { [{ id: 1 }] }
+
+        result = repo.fetch
+        expect(result.first.id).to eq(1)
+      end
+
+      it 'fetches data wihtout' do
+        adapter = double
+        repository_class.adapter = adapter
+
+        repo = repository_class.new
+        repo.select(:id).from(0).to(5).order(:id, :asc)
+
+        expect(adapter).to receive(:get_records).with({
+          "selectColumns" => "MyModule(ID)",
+          "fromIndex" => 0,
+          "toIndex" => 5,
+          "sortColumnString" => "ID",
+          "sortOrderString" => "asc"
+        }) { [{ id: 1 }] }
+
+        result = repo.fetch
+        expect(result.first.id).to eq(1)
+      end
+    end
+
+    context 'aliases' do
+      it 'fetches and returns the first' do
+        records = [double]
+
+        repo = repository_class.new
+        allow(repo).to receive(:fetch) { records }
+
+        expect(repo.first).to eq(records.first)
+      end
+
+      it 'fetches though #to_a' do
+        repo = repository_class.new
+        expect(repo.method(:to_a)).to eq(repo.method(:fetch))
+      end
+    end
+  end
+
+  private
+
+  def repository_class
+    repositoy_model_class = model_class
+
+    @repository_class ||= Class.new do
+      include ZohoCrm::Repositories::Records
+      model repositoy_model_class
+    end
+  end
+
+  def model_class
+    @model_class ||= Class.new do
+      include ZohoCrm::Model
+      custom_module_name 'MyModule'
+
+      property :id, as: 'ID'
+    end
+  end
+end
